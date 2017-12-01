7a302160a2fda652bd9afc5e5a8977ae6b51acd2
nashirsch <nashirsch@uchicago.edu>
2017-11-30 23:25:52 -0600

    okay

--- mproxy.py
+++ mproxy.py
@@ -5,19 +5,39 @@ import threading
 import http.server
 
 
-def requestHandler(client, server, request):
+def requestHandler(client):
 
-        server.send(request)
+        client[0].settimeout(None)
 
-        ret = server.recv(1024)
-        while len(ret) > 1:
+        request = client[0].recv(1024)
+
+        requestedServer = (str(request).split(' ')[1].split(':')[1].replace("/", ""), 80)
+        try:
+            requestedServer[1] = str(request).split(' ')[1].split(':')[2]
+        except:
+            pass
+
+        server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        server.connect(requestedServer)
+
+        if(args.timeout > 0):
+            server.settimeout(args.timeout)
+        else:
+            server.settimeout(None)
+
+        while len(ret) > 0:
             print("recieving packet")
 
             print(len(ret))
 
-            client.send(ret)
+            if str(request)[2:9] != "CONNECT":
 
-            ret = server.recv(1024)
+                client.send(ret)
+
+                ret = server.recv(1024)
+
+        client[0].close()
+        server.close()
 
 
 def main(args):
@@ -32,48 +52,15 @@ def main(args):
     clientSocket.bind((socket.gethostbyname(socket.gethostname()), args.port))
     clientSocket.listen(1)
 
+    while True:
+        client = clientSocket.accept()
 
-    client = clientSocket.accept()
-    client[0].settimeout(None)
-
-    request = client[0].recv(1024)
-
-    while len(request) > 0:
-
-        if str(request)[2:9] != "CONNECT":
-
-            requestedServer = (str(request).split(' ')[1].split(':')[1].replace("/", ""), 80)
-            try:
-                requestedServer[1] = str(request).split(' ')[1].split(':')[2]
-            except:
-                pass
-
-            print(request)
-            print(requestedServer)
-
-            if (requestedServer not in serv[1] for serv in currentServers):
-
-                server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-                server.connect(requestedServer)
-
-                if(args.timeout > 0):
-                    server.settimeout(args.timeout)
-                else:
-                    server.settimeout(None)
-
-                currentServers.append((server, requestedServer))
-
-            else:
-                server = [i[0] for i in currentServers if i[1] is requestedServer]
-
-            if threading.activeCount() <= args.numworkers:
-                t = threading.Thread(target = requestHandler, args = (client[0], server, request))
-                t.start()
+        if threading.activeCount() <= args.numworkers:
+            t = threading.Thread(target = requestHandler, args = (client))
+            t.start()
 
 
-            request = client[0].recv(1024)
-
-    client[0].close()
+    clientSocket.close()
     print("closing")
     print(threading.active_count())
 
